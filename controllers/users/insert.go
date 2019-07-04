8737442680f8e6bdf6baba5d19abc5e9cf82b24b
Your Name <you@example.com>
2019-07-04 11:19:26 +0700

    insert from json success

--- controllers/users/insert.go
+++ controllers/users/insert.go
@@ -3,24 +3,32 @@ package users
 import (
 	"github.com/gin-gonic/gin"
 	"../../models"
+	"fmt"
 )
 
 func Insert(c *gin.Context) {
 	db:= models.DBConn()
-	db.DB().Ping()
 	type User struct {
 		ID   int
 		Name string
 	  }
-	db.Create(&User{Name:"Kinny"})
-	if err := db.Create(&User{Name:"Kinny 123"}).Error; err != nil {
+	type CreateUser struct {
+		Name string `json:"name" binding:"required"`
+	}  
+	var json CreateUser
+	if err:= c.ShouldBindJSON(&json); err!=nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	user := User{Name:json.Name}  //db.Create(&User{Name:"Kinny 123"})
+	if err := db.Create(&user).Error; err != nil {
 		c.JSON(500, gin.H{
 			"message":err,
 		})
 		return 
 	 }
-
+    fmt.Println(user)
 	c.JSON(200, gin.H{
-		"message": "Inserted",
+		"success": user,
 	})
 }
