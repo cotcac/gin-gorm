49f46635adb5046238c052126e9e34c6f600fc5e
Your Name <you@example.com>
2019-07-09 15:45:53 +0700

    clean up insert article

--- controllers/articles/insert.go
+++ controllers/articles/insert.go
@@ -11,27 +11,12 @@ import (
 func Insert(c *gin.Context) {
 	db := models.DBConn()
 
-	// type CreateArticle struct {
-	// 	Title      string            `json:"title" binding:"required,min=3,max=15"`
-	// 	UserID     int               `json:"user_id" binding:"required"`
-	// 	Categories []models.Category `json:"categories" binding:"required"`
-	// }
-
 	var json models.Article
-	// var category models.Category
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(422, gin.H{"error": err.Error()})
 		return
 	}
-	// return
-	// get from database
-
-	// golang:= models.Category{Id:1, Title:"Nodejs"}
-	// nodejs := models.Category{Id:2,Title:"Mongodb"}
-
-	// end get category.
-	article := models.Article{Title: json.Title, Content: json.Content, UserID: json.UserID, Categories: json.Categories} //Categories:[]models.Category{golang,nodejs}
-	// db.Set("gorm:association_autoupdate", false).Create(&article)
+	article := models.Article{Title: json.Title, Content: json.Content, UserID: json.UserID, Categories: json.Categories}
 	if err := db.Create(&article).Error; err != nil {
 		c.JSON(500, gin.H{
 			"message": err,
@@ -40,6 +25,6 @@ func Insert(c *gin.Context) {
 	}
 	fmt.Println(article)
 	c.JSON(200, gin.H{
-		"success": article,
+		"success": article.ID,
 	})
 }
